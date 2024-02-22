50562d1575c0a9e1efa7511ccbbd042b429f30c6
woodawon <woodawon0602@gmail.com>
2024-02-23 00:03:40 +0900

    02/23

--- Library_Management_System.py
+++ Library_Management_System.py
@@ -11,15 +11,20 @@ class Library:
     price: List[float] = None
 
 library = Library()
-ar_nm = List[chr]
-bk_nm = List[chr]
+library.book_name = []
+library.author = []
+library.pages = []
+library.price = []
+
+ar_nm = []
+bk_nm = []
 
 # 사용자 수, 서비스 선택, 책 개수
 u = 0 
-number = 0 
+number = '' 
 count = 0  
 
-while(number != 5):
+while(number != '5'):
     print("Welcome to Library! \n")
     print("1. Add book information \n")
     print("2. Display book information \n")
@@ -28,18 +33,18 @@ while(number != 5):
     print("5. Exit \n")
     number = input("Enter one of the above: ")
 
-    match (number):
-        case (1):
-            library.book_name.add(input("Enter book name : "))
-            library.author.add(input("Enter author name : "))
-            library.pages.add(input("Enter Pages : "))
-            library.price.add(input("Enter price : "))
-        case (2):
+    match number:
+        case '1':
+            library.book_name.append(input("Enter book name : "))
+            library.author.append(input("Enter author name : "))
+            library.pages.append(int(input("Enter Pages : ")))
+            library.price.append(float(input("Enter price : ")))
+        case '2':
             print("Fizz")
-        case (3):
+        case '3':
             print("Buzz")
-        case (4):
+        case '4':
             print("Buzz")
-        case (5):
+        case '5':
             sys.exit("Program is End.")
 
